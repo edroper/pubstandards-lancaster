4734085c525153c930e9b6a8c1b9b6f422a2fbd5
Luke Demi <lddemi@gmail.com>
2023-01-12 12:52:13 -0500

    temporary for jan 12

---
+++ _next/static/ZOrj0KOE_fGuPsmGjBqOy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/28-2bc9056b5f6b9360.js","static/chunks/pages/index-eccaf59e700d4b68.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
