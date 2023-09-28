c7f8de0a6ce7d8d2f569324551470959491ce62b
Luke Demi <lddemi@gmail.com>
2023-09-28 18:46:32 +0000

    push special nov 9

---
+++ _next/static/e78c51H5a7Vuh-Axn9a_O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/382-e90fb54bff5ef490.js","static/chunks/pages/index-929ff628fffa7438.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
