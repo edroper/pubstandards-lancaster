0a07b214160594dd3ff37f114656a660fd0e73b3
Luke Demi <lddemi@gmail.com>
2023-09-28 18:30:50 +0000

    update for hiatus

---
+++ _next/static/kfmSABtXpOil2DUfZefep/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/28-2bc9056b5f6b9360.js","static/chunks/pages/index-c88f7814282c6665.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
